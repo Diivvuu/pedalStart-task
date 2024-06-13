6bd78aa6e932122bde593db7fd58ab8e7d599815
Diivvuu <divyanshubawa9@gmail.com>
2024-06-13 22:40:10 +0530

    tailwind installed

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Routes, Route, Navigate, Outlet, useLocation } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <main className="w-full min-h-screen bg-red-400">
+      <h1>Login</h1>
+      <Routes></Routes>
+    </main>
+  );
+};
+
+export default Login;
