5c35b4cd9501a38926c8e26b1a3d8ecd81c709dc
Diivvuu <divyanshubawa9@gmail.com>
2024-06-14 14:15:39 +0530

    signin buttons

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const mongoURI = "mongodb://127.0.0.1:27017/task-manager";
+const mongoURI = process.env.MONGODB_URI; // Use environment variable for MongoDB URI
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
