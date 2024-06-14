344ca9bd3b0177396635f11746ed186e5afcb9ec
Diivvuu <divyanshubawa9@gmail.com>
2024-06-14 13:13:12 +0530

    dashboard styling

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -1,53 +1,58 @@
-import Sidebar from '../../components/sidebar/Sidebar';
-import { Link } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import './dashboard.scss';
-import { useEffect } from 'react';
-import { getAllTasks } from '../../redux/taskSlice';
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import "./dashboard.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import { getAllTasks } from "../../redux/taskSlice";
+
 const Dashboard = () => {
-	const tasklist = useSelector((state) => state.task);
-	const { AllTasks } = tasklist;
-	const user = useSelector((state) => state.auth);
-	const { currentUser } = user;
+  const tasklist = useSelector((state) => state.task);
+  const { AllTasks } = tasklist;
+  const user = useSelector((state) => state.auth);
+  const { currentUser } = user;
 
-	let pendingTask = [];
-	let completedTask = [];
-	for (let i = 0; i < AllTasks.length; i++) {
-		if (AllTasks[i].status === 'todo') {
-			pendingTask.push(AllTasks[i]);
-		} else if (AllTasks[i].status === 'done') {
-			completedTask.push(AllTasks[i]);
-		}
-	}
+  let pendingTask = [];
+  let completedTask = [];
+  for (let i = 0; i < AllTasks.length; i++) {
+    if (AllTasks[i].status === "todo") {
+      pendingTask.push(AllTasks[i]);
+    } else if (AllTasks[i].status === "done") {
+      completedTask.push(AllTasks[i]);
+    }
+  }
 
-	const dispatch = useDispatch();
-	useEffect(() => {
-		dispatch(getAllTasks(currentUser.token, currentUser.id));
-	}, [dispatch, currentUser.token, currentUser.id]);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllTasks(currentUser.token, currentUser.id));
+  }, [dispatch, currentUser.token, currentUser.id]);
 
-	return (
-		<div>
-			<div className='dashboard'>
-				<div className='dashboard__left'>
-					<Sidebar />
-				</div>
-				<div className='dashboard__right'>
-					<div className='dashboard__rightContent'>
-						<h2>Task Status Dashboard</h2>
-						<div className='taskcount'>
-							<div className='todo box'>Todo - {pendingTask.length}</div>
-							<div className='done box'>Complete - {completedTask.length}</div>
-						</div>
-						<div className='createButton'>
-							<Link to='/taskmanager' className='button'>
-								Create Task
-							</Link>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="dashboard">
+      <div className="dashboard__left">
+        <Sidebar />
+      </div>
+      <div className="dashboard__right">
+        <div className="dashboard__content">
+          <h2 className="dashboard__title">Task Status Dashboard</h2>
+          <div className="dashboard__stats">
+            <div className="dashboard__stat">
+              <div className="stat__count">{pendingTask.length}</div>
+              <div className="stat__label">Todo Tasks</div>
+            </div>
+            <div className="dashboard__stat">
+              <div className="stat__count">{completedTask.length}</div>
+              <div className="stat__label">Completed Tasks</div>
+            </div>
+          </div>
+          <div className="dashboard__actions">
+            <Link to="/taskmanager" className="button button-primary">
+              Create New Task
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
